5dbe2d13d6fe53257c0ba9451d33370830864658
ajinkya.chanshetty <ajinkya.chanshetty@gmail.com>
2018-01-08 18:31:29 +0530

    Added the first phase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgSemanticModule } from 'ng-semantic';
+
 
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgSemanticModule
   ],
   providers: [],
   bootstrap: [AppComponent]
